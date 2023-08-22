df31f87168162134ce4c4a6b7b810d5a674d7e60
Rorie Reyes <64812632+rreyes-RP@users.noreply.github.com>
2023-08-22 01:09:14 -0400

    Starting the 100 days of Python course

---
+++ Python_Projects/100DaysOfPython/Day1/bandNameGenerator.py
@@ -0,0 +1,5 @@
+print("Hello. Welcome to the Band Name Generator app. I'll ask you a few questions")
+city = input("What city did you grow up in? ")
+petName = input("What is the name of your pet? ")
+bandName = city + " " + petName
+print("Your band name is " + bandName)
---
+++ Python_Projects/100DaysOfPython/Day1/printLesson.py
@@ -0,0 +1,29 @@
+# print("Hello world!\nHello world!\nMy name is Rorie.") 
+
+# Concatenate: combining strings
+
+# print("Hello" + " Rorie")
+
+# Input function
+# input("Prompt for user")
+# program is paused until the user inputs data
+# nested input inside print
+
+# print("Hello " + input("What is your name? "))
+
+# variables
+
+'''name = input("What is your name? ")
+print(name)
+
+name1 = "Jack"
+print(name1)
+
+name1 = "Angela"
+print(name1)
+
+length = len(name)
+print(length)'''
+
+time_until_midnight = "5"
+print("There are "+time_until_midnight+" hours until midnight")
