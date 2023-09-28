70783587b9be628a376e1e0c222968c63464b8b0
Rorie Reyes <64812632+rreyes-RP@users.noreply.github.com>
2023-09-28 11:47:29 -0400

    Importing all changes

--- Python_Projects/100DaysOfPython/Day4/learnLists.py
+++ Python_Projects/100DaysOfPython/Day4/learnLists.py
@@ -16,3 +16,12 @@ us_states.append("Angelaland")
 us_states.extend(["Rorieville", "Jeaninetown"])
 
 print(us_states)
+
+# Nested lists
+
+#dirty_dozen = ["Strawberries", "Spinach", "Kale", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears", "Tomatoes", "Celery", "Potatoes"]
+fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+dirty_dozen = [fruits, vegetables]
+
+print(dirty_dozen[1][1])
