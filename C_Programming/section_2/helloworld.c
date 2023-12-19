4ac9ac6800a8a007892f3b891839175cb6f0c3c4
Rorie Reyes <64812632+rreyes-RP@users.noreply.github.com>
2023-12-19 10:30:37 -0500

    Practicing C Programming

---
+++ C_Programming/section_2/helloworld.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/* This program displays any
+ * arguments that were passed to it*/
+
+int main (int argc, char **argv) {
+    int i;
+    for (i = 0; i < argc; i++) {
+        printf("Hello World! argc = %d arg %d is %s\n", argc, i, argv[i]);
+    }
+    return 0;
+}
